ec8305f0331b2d4a83570902e9a749db0a3e198a
Joao Natividade <joao.natividade01@etec.sp.gov.br>
2025-06-27 23:06:16 -0300

    Add app/__init__.py 1751076376007

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,9 @@
-# Application initialization moved to app.py
-# This file remains for package initialization only
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_restx import Api
+
+db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
+api = Api(version='1.0', title='TaskFlow API', description='A Trello-like backend API for TaskFlow')
