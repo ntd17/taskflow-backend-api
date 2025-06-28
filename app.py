3472c512f67cfc319ff8a622a464b47c1494acfb
Joao Natividade <joao.natividade01@etec.sp.gov.br>
2025-06-27 22:58:54 -0300

    Add app.py 1751075933678

--- app.py
+++ app.py
@@ -8,6 +8,3 @@ migrate.init_app(app, db)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-# Gunicorn expects a variable named 'app' in this file to run the app
-# The create_app() returns the Flask app instance, so assign it to 'app'
